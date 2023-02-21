6794768e06e091a27b135172d2edb4e7bcfd496f
wangxuwen <wangxuwen@staff.sinaft.com>
2023-02-21 18:22:35 +0800

    nio

---
+++ test-project-demo/java-test/src/main/java/demo/nio/NioDemo.java
@@ -0,0 +1,106 @@
+package demo.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class NioDemo {
+
+    //处理 Selector 中的IO事件
+    public static void runServerSocketChannel() throws IOException {
+        //创建一个Selector
+        Selector selector = Selector.open();
+        //创建一个服务端的ServerSocketChannel
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        //设置是否阻塞
+        channel.configureBlocking(false);
+        //绑定套接字，并监听套接字
+        channel.bind(new InetSocketAddress(18899));
+        //将Channel注册到Selector上, 指定选择器要监听的IO事件类型
+        //ServerSocketChannel 只能监听 OP_ACCEPT 事件
+        channel.register(selector, SelectionKey.OP_ACCEPT);
+
+        //轮询Selector，当有IO事件时执行对应操作
+        //select() 阻塞调用，直到有一个Channel发生了注册的IO事件
+        while (selector.select() > 0) {
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                //触发接收事件
+                if (key.isAcceptable()) {
+                    System.out.println("触发接收事件：OP_ACCEPT");
+                    //创建一个缓冲区
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    //获取 key 对应的 Channel
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    //将数据读入到缓冲区
+                    int length = socketChannel.read(byteBuffer);
+                    System.out.println(new String(byteBuffer.array(), 0, length));
+
+                }
+                //移除已被处理的事件
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void runServer_OP_READ() throws IOException {
+        //创建一个Selector
+        Selector selector = Selector.open();
+        //创建一个服务端的ServerSocketChannel
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        //设置是否阻塞
+        channel.configureBlocking(false);
+        //绑定套接字，并监听套接字
+        channel.bind(new InetSocketAddress(18899));
+        //将Channel注册到Selector上, 指定选择器要监听的IO事件类型
+        //ServerSocketChannel 只能监听 OP_ACCEPT 事件
+        channel.register(selector, SelectionKey.OP_ACCEPT);
+
+        //轮询Selector，当有IO事件时执行对应操作
+        //select() 阻塞调用，直到有一个Channel发生了注册的IO事件
+        while (selector.select() > 0) {
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = selectionKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                //触发接收事件
+                if (key.isAcceptable()) {
+                    System.out.println("触发接收事件：OP_ACCEPT");
+                    //获取 key 对应的 Channel
+                    ServerSocketChannel serverSocketChannel = (ServerSocketChannel) key.channel();
+                    SocketChannel socketChannel = serverSocketChannel.accept();
+                    socketChannel.configureBlocking(false);
+                    /**
+                        在此处绑定一个监控 OP_READ 的 SocketChannel，当之后的数据接收的时候，就能触发写事件
+                     */
+                    socketChannel.register(selector, SelectionKey.OP_READ);
+                } else if (key.isReadable()) {
+                    System.out.println("触发写事件：OP_READ");
+                    //创建一个缓冲区
+                    ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                    //获取 key 对应的 Channel
+                    SocketChannel socketChannel = (SocketChannel) key.channel();
+                    socketChannel.configureBlocking(false);
+                    //将数据读入到缓冲区
+                    int length = socketChannel.read(byteBuffer);
+                    System.out.println(new String(byteBuffer.array(), 0, length));
+                }
+                //移除已被处理的事件
+                iterator.remove();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        runServer_OP_READ();
+    }
+
+}
---
+++ test-project-demo/java-test/src/main/java/test/Test.java
@@ -0,0 +1,32 @@
+package test;
+
+import demo.nio.NioDemo;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Test {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            Socket socket = new Socket("127.0.0.1", 18899);
+            OutputStream outputStream = socket.getOutputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println(line);
+            printWriter.flush();
+            socket.shutdownOutput();
+
+        }
+    }
+
+}
