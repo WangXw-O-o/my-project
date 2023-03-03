2f41fecda878c18467b1083a2b31d6ccd4149576
wangxuwen <wangxuwen@staff.sinaft.com>
2023-03-03 15:34:29 +0800

    netty demo

--- test-project-demo/java-test/src/main/java/demo/io/netty/NettyDiscardServer.java
+++ test-project-demo/java-test/src/main/java/demo/io/netty/NettyDiscardServer.java
@@ -18,26 +18,36 @@ public class NettyDiscardServer {
     }
 
     public void runServer() {
-        EventLoopGroup boss = new NioEventLoopGroup();
+        //boss轮询组
+        EventLoopGroup boss = new NioEventLoopGroup(1);
+        //worker轮询组
         EventLoopGroup worker = new NioEventLoopGroup();
         try {
+            //将轮询组加入到引导类中
             bootstrap.group(boss, worker);
+            //设置通道的IO类型。NioServerSocketChannel：nio的TCP服务端通道
             bootstrap.channel(NioServerSocketChannel.class);
+            //设置监听端口
             bootstrap.localAddress(serverPort);
+            //设置通道的参数。SO_KEEPALIVE：心跳机制
             bootstrap.option(ChannelOption.SO_KEEPALIVE, true);
+            //装配子通道的Pipeline
             bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel socketChannel) throws Exception {
                     socketChannel.pipeline().addLast(new NettyDiscardHandler());
                 }
             });
+            //绑定端口。sync()阻塞，直到绑定成功才返回。
             ChannelFuture channelFuture = bootstrap.bind().sync();
             System.out.println("服务器启动成功！监听端口：" + channelFuture.channel().localAddress());
+            //自我阻塞，直到通道关闭的异步任务结束
             ChannelFuture closeFuture = channelFuture.channel().closeFuture();
             closeFuture.sync();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
+            //释放资源
             boss.shutdownGracefully();
             worker.shutdownGracefully();
         }
