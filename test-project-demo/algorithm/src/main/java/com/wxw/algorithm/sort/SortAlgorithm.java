3536a7fb4aafa2abec0358d7267841c811bbcce5
wangxuwen <wangxuwen@staff.sinaft.com>
2023-03-17 19:37:02 +0800

    算法

---
+++ test-project-demo/algorithm/src/main/java/com/wxw/algorithm/sort/SortAlgorithm.java
@@ -0,0 +1,30 @@
+package com.wxw.algorithm.sort;
+
+public class SortAlgorithm {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 冒泡排序
+     */
+    public static int[] mao_pao_sort(int[] target) {
+        if (target == null) {
+            return null;
+        }
+        if (target.length < 2) {
+            return target;
+        }
+
+        int length = target.length;
+        for (int i = 0; i < length; i++) {
+            boolean isChange = true;
+            int p = target[0];
+            int next = target[1];
+        }
+
+        return null;
+    }
+
+}
